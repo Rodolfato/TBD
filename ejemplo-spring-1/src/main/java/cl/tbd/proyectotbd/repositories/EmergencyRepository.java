067e3b52fae7ef23197e85db43d8d007618e49aa
Rodolfo Montana <rodolfo.montana@usach.cl>
2020-11-21 18:21:09 -0300

    Eliminado ejemplo-spring-1

--- ejemplo-spring-1/src/main/java/cl/tbd/proyectotbd/repositories/EmergencyRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.tbd.proyectotbd.repositories;
-import java.util.List;
-import cl.tbd.proyectotbd.models.Emergency;
-
-public interface EmergencyRepository {
-    public int countEmergencies();
-    public List<Emergency> getAllEmergencies();
-    public Emergency createEmergency(Emergency emergency);    
-}
